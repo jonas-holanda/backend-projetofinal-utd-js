08e433b7ee7693ab270dd2cf9360eef923c1cf4d
Jonas Holanda <jonasholanda15@gmail.com>
2024-03-13 16:23:07 -0300

    fix: solução de erro 500

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectModule: require('mysql2'),
     operatorsAliases: false,
 });
 
